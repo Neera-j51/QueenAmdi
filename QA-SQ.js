9d1c6c694529c172d576fa6b12efde7d7649e9d3
Joker adanali gg <101932608+Neera-j51@users.noreply.github.com>
2022-03-27 15:30:39 +0300

    Update QA-SQ.js

--- QA-SQ.js
+++ QA-SQ.js
@@ -1,5 +1,5 @@
 module.exports = {
-    QUEEN_AMDI_OWNER: '94757405652', // Black Amda - Queen Amdi Owner
-    QUEEN_AMDI_TEAM: '94719077818,94757672873,94774976567,94766426385,94711870791', // Team - Queen Amdi TEAM
-    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+    QUEEN_AMDI_OWNER: '12012011513', // Black Amda - Queen Amdi Owner
+    QUEEN_AMDI_TEAM: '254772838910,905379649077,12012011513,254772838910,905379649077', // Team - Queen Amdi TEAM
+    SEW_QUEEN_OWNER: '905379649077' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
 }; 
